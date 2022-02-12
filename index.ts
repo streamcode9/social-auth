952618723277310e83bbdb0577db4b3107a01d15
Alexander <streamcode9@users.noreply.github.com>
2022-02-12 21:53:42 +0100

    new: auth0 hello world

--- index.ts
+++ index.ts
@@ -1,11 +1,29 @@
 import express from 'express';
+import { auth, requiresAuth } from 'express-openid-connect';
 
 const app = express()
 const port = 3000
 
-app.get('/', (req: any, res: any) => {
-	res.send('Hello World!')
-})
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'keyVault-value',
+  baseURL: 'http://localhost:3000',
+  clientID: 'keyVault-value',
+  issuerBaseURL: 'https://dev-025d9prn.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`)
