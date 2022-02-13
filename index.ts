5781c371599cfe41ce96cdac2167cbc27a6c1b82
Alexander <streamcode9@users.noreply.github.com>
2022-02-13 22:41:12 +0100

    chore: auth0 middleware

--- index.ts
+++ index.ts
@@ -1,12 +1,10 @@
 import express from 'express';
-import { auth, requiresAuth } from 'express-openid-connect';
+import { auth } from 'express-openid-connect';
 
 const app = express()
 const port = 3000
 
-const router = express.Router()
-
-const config = {
+const auth0Config = {
 	authRequired: false,
 	auth0Logout: true,
 	secret: 'keyVault-value',
@@ -15,22 +13,25 @@ const config = {
 	issuerBaseURL: 'https://dev-025d9prn.us.auth0.com'
 }
 
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config))
-
-// invoked for any requests passed to this router
-router.use((req, res, next) => {
-	if (!req.oidc.isAuthenticated() && !['/login', '/logout', '/callback'].includes(req.path)) {
-		res.status(401).end()
-	}
-	next()
-})
-
+const router = express.Router()
 router.get('/profile', (req, res, next) => {
+	console.log('profile')
 	res.send(JSON.stringify(req.oidc.user));
 	next()
 })
 
+const auth0Middleware = (req: any, res: any, next: any) => {
+	console.log('auth middleware', req.path)
+	if (!req.oidc.isAuthenticated() && !['/login', '/logout', '/callback'].includes(req.path)) {
+		res.status(401).end()
+	} else {
+		next()
+	}
+}
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(auth0Config))
+app.use(auth0Middleware);
 app.use('/', router)
 
 app.listen(port, () => {
