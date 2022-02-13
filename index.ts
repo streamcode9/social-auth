b63bfd42ff8f18346d6f702621b1fcff7479ce7b
Alexander <streamcode9@users.noreply.github.com>
2022-02-13 21:38:43 +0100

    chore: use router

--- index.ts
+++ index.ts
@@ -4,26 +4,34 @@ import { auth, requiresAuth } from 'express-openid-connect';
 const app = express()
 const port = 3000
 
+const router = express.Router()
+
 const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: 'keyVault-value',
-  baseURL: 'http://localhost:3000',
-  clientID: 'keyVault-value',
-  issuerBaseURL: 'https://dev-025d9prn.us.auth0.com'
-};
+	authRequired: false,
+	auth0Logout: true,
+	secret: 'keyVault-value',
+	baseURL: 'http://localhost:3000',
+	clientID: 'keyVault-value',
+	issuerBaseURL: 'https://dev-025d9prn.us.auth0.com'
+}
 
 // auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
+app.use(auth(config))
 
-// req.isAuthenticated is provided from the auth router
-app.get('/', (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-});
+// invoked for any requests passed to this router
+router.use((req, res, next) => {
+	if (!req.oidc.isAuthenticated() && !['/login', '/logout', '/callback'].includes(req.path)) {
+		res.status(401).end()
+	}
+	next()
+})
 
-app.get('/profile', requiresAuth(), (req, res) => {
-  res.send(JSON.stringify(req.oidc.user));
-});
+router.get('/profile', (req, res, next) => {
+	res.send(JSON.stringify(req.oidc.user));
+	next()
+})
+
+app.use('/', router)
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`)
